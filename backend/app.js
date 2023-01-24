7049d68d3f9996361de3283a94281ece0d23ecfa
AlexanderMolokanov <g.mkv@yandex.ru>
2023-01-24 23:44:36 +0300

    упростить корс

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,8 @@ const options = {
   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
   credentials: true,
 };
-app.use('*', cors(options));
+// app.use('*', cors(options));
+app.use(cors())
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
