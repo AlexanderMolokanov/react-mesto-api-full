bee78f5bee448621304a6ca2aae73962e08bea4d
AlexanderMolokanov <g.mkv@yandex.ru>
2023-02-03 19:17:32 +0300

    поправить логгер

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,6 @@ const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-// const cors = require('./middlewares/cors');
 const { handleError } = require('./errors/handleError');
 const NotFoundError = require('./errors/NotFoundError');
 const routes = require('./routes');
@@ -17,61 +16,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, NODE_ENV, MONGODB_ADDRESS } = process.env;
 
-// const options = {
-//   origin: [
-//   'http://gmkv.nomoredomains.rocks',
-//   'https://gmkv.nomoredomains.rocks',
-//   'http://api.gmkv.nomoredomains.rocks',
-//   'https://api.gmkv.nomoredomains.rocks',
-//   'http://localhost:3000',
-//   'https://localhost:3000',
-//   'http://localhost:3001',
-//   'https://localhost:3001',
-//   'http://84.201.132.131',
-//   'https://84.201.132.131',
-//   'http://api.84.201.132.131',
-//   'https://api.84.201.132.131',
-//   'http://api.84.201.132.131/users/me',
-//   'https://api.gmkv.nomoredomains.rocks/users/me',
-//   ],
-//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-//   credentials: true,
-// };
-// app.use('*', cors(options));
-// app.use(cors(options))
-
-//     app.use((req, res, next) => {
-//       res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-//   if (req.method === 'OPTIONS') {
-//     res.send(200);
-//   }
-//   next();
-// });
-
 app.use(cors({ credentials: true, origin: true }));
-
-// app.use((req, res, next) => {
-//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-//   // проверяем, что источник запроса есть среди разрешённых
-//   const { methodHttp } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-//   const requestHeaders = req.headers["access-control-request-headers"];
-//   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-//   if (allowedCors.includes(origin)) {
-//     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-//     res.header("Access-Control-Allow-Origin", origin);
-//   }
-//   if (methodHttp === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-//     res.header("Access-Control-Allow-Headers", requestHeaders);
-//     return res.end();
-//   }
-//   return next();
-// });
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -90,12 +35,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 mongoose.set('strictQuery', false);
-// mongoose.connect('mongodb://localhost:27017/mestodb', {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-//   useUnifiedTopology: true,
-// })
 
 app.get('/crash-test', () => {
   setTimeout(() => {
